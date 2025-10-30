9166300ba6e2de1d87794652230a4c0bf2fe517c
pankajkoree <pankajkori2057@gmail.com>
2025-10-30 21:29:59 +0545

    deployment problem fixing - 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@
 import "./globals.css";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function RootLayout({
   children,
