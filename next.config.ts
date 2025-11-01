1eab11e5bdc409cea2c65eba5947d156c7a62aa1
pankajkoree <pankajkori2057@gmail.com>
2025-11-01 10:02:42 +0545

    image domain fixed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["userpic.codeforces.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "userpic.codeforces.org",
+      },
+    ],
   },
 };
 
