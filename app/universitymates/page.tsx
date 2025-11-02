f846c3c0bde7e5a73398556fc84bc255509ac147
pankajkoree <pankajkori2057@gmail.com>
2025-11-02 10:12:59 +0545

    working on university mates

---
+++ app/universitymates/page.tsx
@@ -0,0 +1,11 @@
+const universitymates = () => {
+    return (
+        <div className="max-w-full">
+            <main className="p-6 mx-auto md:w-[70%]">
+                <h1>universitymates</h1>
+            </main>
+        </div>
+    )
+}
+
+export default universitymates
