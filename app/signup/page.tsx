cc8bc6d3be5448cb7d1ae17ff0f0b76024353041
pankajkoree <pankajkori2057@gmail.com>
2025-11-05 10:12:11 +0545

    signup ui

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -29,18 +29,18 @@ const Signup = () => {
                 </div>
 
                 {/* signup form */}
-                <form action="" className="flex flex-col border border-red-300 gap-2 text-black">
+                <form action="" className="flex flex-col gap-2 text-black w-[30%]">
                     {/* firstname */}
-                    <div className="relative">
+                    <div className="flex top-4 relative">
                         <Input
                             type="text"
                             id="firstname"
                             required
-                            className="peer mt-2.5 w-full pt-0 tracking-[1px] text-[#6a2020] mb-[30px] border-b border-[#0c0909] outline-none bg-transparent focus:border-[#55189f] transition-colors duration-300"
+                            className="peer mt-4 w-full pt-0 tracking-[1px] text-[#6a2020] mb-[30px] border-2 border-blue-200 outline-none bg-transparent focus:border-[#55189f] transition-colors duration-300 rounded-sm"
                         />
                         <Label
                             htmlFor="firstname"
-                            className="absolute left-0 top-2.5 text-[16px] text-[#6a2020] pointer-events-none transition-all duration-500 peer-focus:-top-5 peer-focus:text-[14px] peer-focus:text-[#55189f] peer-valid:-top-5 peer-valid:text-[14px] peer-valid:text-[#55189f]"
+                            className="absolute left-2 top-7 text-[#6a2020] pointer-events-none transition-all duration-500 peer-focus:-top-3 peer-focus:text-[#55189f] peer-valid:-top-5 peer-valid:text-[#55189f]"
                         >
                             First Name
                         </Label>
@@ -52,15 +52,16 @@ const Signup = () => {
                             type="text"
                             id="firstname"
                             required
-                            className="peer mt-2.5 w-full pt-0 tracking-[1px] text-[#6a2020] mb-[30px] border-2 border-blue-200 outline-none bg-transparent focus:border-[#55189f] transition-colors duration-300"
+                            className="peer mt-4 w-full pt-0 tracking-[1px] text-[#6a2020] mb-[30px] border-2 border-blue-200 outline-none bg-transparent focus:border-[#55189f] transition-colors duration-300 rounded-sm"
                         />
                         <Label
                             htmlFor="firstname"
-                            className="absolute left-0 top-2.5 text-[16px] text-[#6a2020] pointer-events-none transition-all duration-500 peer-focus:-top-5 peer-focus:text-[14px] peer-focus:text-[#55189f] peer-valid:-top-5 peer-valid:text-[14px] peer-valid:text-[#55189f]"
+                            className="absolute left-2 top-7 text-[#6a2020] pointer-events-none transition-all duration-500 peer-focus:-top-3 peer-focus:text-[#55189f] peer-valid:-top-5 peer-valid:text-[#55189f]"
                         >
                             First Name
                         </Label>
                     </div>
+
 
 
                     <Button>Register</Button>
