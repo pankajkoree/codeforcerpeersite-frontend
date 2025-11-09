aa20f71d8e84a24f6604247464ec4caf272d1385
pankajkoree <pankajkori2057@gmail.com>
2025-11-09 18:51:52 +0545

    auth provider

--- app/ClientWrapper.tsx
+++ app/ClientWrapper.tsx
@@ -7,17 +7,20 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "@/context/AuthContext";
 const ClientWrapper = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <div className="antialiased text-gray-600 min-h-screen flex flex-col bg-linear-to-r from-blue-100 via-green-50  to-blue-100">
       <QueryClientProvider client={queryClient}>
-        <Header />
+        <AuthProvider>
+          <Header />
 
-        {/* Main content grows to fill available space */}
-        <main className="flex-1">{children}</main>
-        <Toaster position="top-right" reverseOrder={false} />
-        <Footer />
+          {/* Main content grows to fill available space */}
+          <main className="flex-1">{children}</main>
+          <Toaster position="top-right" reverseOrder={false} />
+          <Footer />
+        </AuthProvider>
       </QueryClientProvider>
     </div>
   );
